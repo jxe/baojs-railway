ef14a11634de01c26c27ab285af11ecabb49ce95
Joe Edelman <joe.edelman@gmail.com>
2024-09-18 12:11:01 +0200

    woot

--- lib/scheduling-assistant.ts
+++ lib/scheduling-assistant.ts
@@ -2,11 +2,13 @@ import { type User } from '@prisma/client'
 import { createRunAndPollAndHandleToolCalls, type Assistant, type RunParams, type Tool } from './openai'
 import { z } from 'zod'
 import { db } from './services'
+import { bot } from './bot'
 
 const scheduleSchema = z.object({
   availability: z.string(),
   scheduled: z.string().optional(),
-  buddiedWith: z.number().optional()
+  buddiedWith: z.number().optional().describe('The user ID of the person this user is walking with. This will inform the other user they\'ve been matched.'),
+  messageForBuddy: z.string().optional().describe('A message to send to the buddy this user has been matched with, telling them when and where to meet, and whether they will walk with Joe before or after.'),
 })
 
 export const schedulingAssistant = {
@@ -14,7 +16,7 @@ export const schedulingAssistant = {
   tools: {
     scheduleTool: {
       schema: scheduleSchema,
-      async fn({ availability, buddiedWith, scheduled }, { user }) {
+      async fn({ availability, buddiedWith, scheduled, messageForBuddy }, { user }) {
         await db.user.update({
           where: {
             id: user.id,
@@ -23,7 +25,13 @@ export const schedulingAssistant = {
             availability, scheduled, walkingWithUserId: buddiedWith
           }
         })
-        return `Updated the schedule.`
+
+        if (buddiedWith) {
+          const buddy = await db.user.findUnique({ where: { id: buddiedWith } })
+          await bot.api.sendMessage(buddy.telegramId, `${messageForBuddy}. \n\n(You have been matched with ${user.firstName} ${user.lastName} to go for a walk. Their telegram username is @${user.username}. They are available at ${availability}.)`)
+        } else {
+          return `User has been updated.`
+        }
       }
     } satisfies Tool<z.infer<typeof scheduleSchema>, { user: User }>
   }
--- scripts/update-assistant.ts
+++ scripts/update-assistant.ts
@@ -2,9 +2,19 @@ import zodToJsonSchema from 'zod-to-json-schema'
 import { schedulingAssistant } from '../lib/scheduling-assistant'
 import { openai } from '../lib/services'
 
-const instructions = `Your job is to schedule Joe's birthday. Joe wants to take walks with all his friends, and also have his friends take walks with each other. So just ask users when they are free, and use that info to figure out when they'll walk with Joe, and whether they can also walk with another friend right before or after.
+const instructions = `Your job is to schedule Joe's birthday. Joe wants to take walks with all his friends, and also have his friends take walks with each other. The walks will be from 10a-2:30p on September 28, which is a Saturday. Walks will be along the kanal in Berlin between Löhmuhlenplatz and Mehringdamm.
 
-The walks will be from 10a-2p on September 28, which is a Saturday. Earlier walks will be along the kanal in Berlin between Kotti and Treptow. Later walks will be in hasenheide or tempelhofer feld.`
+You will talk to one friend of Joe’s at a time.
+
+- First, find out when they are free. The “available” column will be set to this.
+- If there’s someone else that’s free then, tell them to walk with each other, and share the other person’s telegram username, if they have one. And their full name. And make up a time and place for their walk, and update the “scheduled” column of both people to match. Tell them that Joe will take a walk with one person after their walk together, and one person before. (So one person needs to be there earlier.) In this case, use the messageForBuddy and buddiedWith fields to send a message to the person they've been matched with to let them know.
+- If no one else is free at that time, go ahead and make up a rough time and place, and tell them they will go for a walk with Joe around them, and you’ll get back to them about taking a walk before or after with a different friend of Joe’s. You’ll make the introduction once you know.
+
+There are some other things the users can do for Joe’s birthday, which you can tell them about:
+
+- At 2:45pm, there is Contemplative Dance Practice at Marameo.
+- There will be a dinner around 6:30pm. Location TBA. They can join the birthday telegram group at https://t.me/+Kjf-Yc5inyI1MDRi to find out.
+- After dinner, there is a party called “Burn Night” which they can google. At that party, Joe is doing a 1h workshop called “Victorian-Style Flirting for Powerful People”. The description is: “This workshop features an interactive game of witty, turn-based conversation where each response must impress and challenge the other to reply in a way that upholds their honor. Practice Victorian-era flirting techniques, conveying subtle meanings through restrained gestures and questions.”`
 
 await openai.beta.assistants.update(
   schedulingAssistant.id,
