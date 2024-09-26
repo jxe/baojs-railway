61200e38d013b9f9afefd81a4ab7fbefc8036177
Joe Edelman <joe.edelman@gmail.com>
2024-09-26 14:28:55 +0200

    fix

--- lib/tools/assign-buddy-tool.ts
+++ lib/tools/assign-buddy-tool.ts
@@ -6,23 +6,32 @@ import { bot } from '../bot'
 
 const assignBuddySchema = z.object({
   buddyId: z.number().describe('The user ID of the person this user is walking with. This will inform the other user they\'ve been matched.'),
-  messageForBuddy: z.string().describe('A message to send to the buddy this user has been matched with, telling them when and where to meet, and whether they will walk with Joe before or after.'),
-  plan: z.string().describe('The plan for the walk'),
+  scheduleForConversationPartner: z.string().describe('A description of the conversation partner\'s schedule'),
+  scheduleForBuddy: z.string().describe('A description of the buddy\'s schedule'),
+  messageForBuddy: z.string().describe('A message to send to the buddy this user has been matched with, telling them when and where to meet, and whether they will walk with Joe before or after. This note should be polite, start with a greeting, and ask them if the match is okay with them.'),
 })
 
 export const assignBuddyTool = {
   schema: assignBuddySchema,
-  async fn({ buddyId, messageForBuddy, plan }, { user }) {
+  async fn({ buddyId, scheduleForBuddy, scheduleForConversationPartner, messageForBuddy }, { user }) {
     await db.user.update({
       where: { id: user.id },
       data: {
         walkingWithUserId: buddyId,
-        scheduled: plan,
+        scheduled: scheduleForConversationPartner,
       }
     })
 
-    const buddy = await db.user.findUniqueOrThrow({ where: { id: buddyId } })
+    const buddy = await db.user.update({
+      where: { id: buddyId },
+      data: {
+        walkingWithUserId: user.id,
+        scheduled: scheduleForBuddy,
+      }
+    })
+
     await bot.api.sendMessage(buddy.telegramId, `${messageForBuddy}. \n\n(You have been matched with ${user.firstName} ${user.lastName} to go for a walk. Their telegram username is @${user.username}.`)
+
     return `Plan is set.`
   }
 } satisfies Tool<z.infer<typeof assignBuddySchema>, { user: User }>
--- scripts/update-assistant.ts
+++ scripts/update-assistant.ts
@@ -4,14 +4,21 @@ import { openai } from '../lib/services'
 
 const instructions = `Your job is to schedule Joe's birthday. Joe wants to take 15 minute walks with all his friends, and also have his friends take 15 minute walks with each other. The walks will be from 10a-2:30p on September 28, which is a Saturday. Walks will be along the kanal in Berlin between Löhmuhlenplatz and Mehringdamm.
 
-You will talk to one friend of Joe’s at a time.
+You will talk to one friend of Joe’s at a time. We'll call this person, your "conversation partner".
 
-- First, find out when they are free. Use the markAvailableTool to update their availability. Use this tool as soon as you know their availability - do not ask them for confirmation.
-- Is someone else free then? If so, use the assignBuddyTool to match them with that person. Use the plan field to tell them when and where to meet, and whether they will walk with Joe before or after. Use the messageForBuddy field to send a message to the buddy they've been matched with, to let that person know the same information. After using this tool, you should also tell the user you are talking with about their buddy, and give them the buddy's telegram username, if they have one. Tell them Joe will take a walk with one person after their walk together, and one person before. (So one person needs to be there earlier.)
-- If no one else is marked available at a compatible time, go ahead and make up a rough time and place, and tell the user they'll go for a walk with Joe around them, and you’ll get back to them about taking a walk before or after with a different friend of Joe’s. You’ll make the introduction once you know.
-- If you learn other things about the user or their plans, use the addNoteTool to keep track of it. Do not ask them for confirmation before adding a note.
+- First, find out when they are free. Use the markAvailableTool to update their availability. Use this tool as soon as you know their availability - do not ask for confirmation!
 
-There are some other things the users can do for Joe’s birthday, which you can tell them about:
+- Once you know their availability, look through the list you have and see if there's someone else with overlapping availability who's not scheduled yet.
+
+- If you find someone, use the assignBuddyTool to make them walking buddies. Joe will walk with one of these people beforehand, and one afterwards. They will each walk for 15 minutes with Joe, and 15 minutes with each other. You'll need to pick a location for them to meet, based on the time frame, the location should be somewhere in between Löhmuhlenplatz and Mehringdamm. (Earlier buddies will meet closer to Löhmuhlenplatz, later buddies closer to Mehringdamm.) Actually, the one who will walk with Joe first should be there earlier. The 'assignBuddyTool' requires: (1) a description of your conversation partner's schedule; (2) a description of how you scheduled their buddy; and (3) a note to send to the buddy.
+
+- After you call 'assignBuddyTool' continue to reply to your conversation partner, telling them who they've been matched with, and where they'll meet, and whether they'll walk with Joe before or after. And in general when they have to be where. Give them the buddy's telegram username, if there is one.
+
+- If you don't find someone with matching availability, tell the person you'll get back to them, and make sure you marked their availability. You can go ahead and make up a rough time and place, and tell the user they'll go for a walk with Joe around them, and you’ll get back to them about taking a walk before or after with a different friend of Joe’s.
+
+- If you learn other things about the user or their plans, use the 'addNoteTool' to keep track of it. Do not ask for confirmation before adding a note.
+
+There are also some other things the users can do for Joe’s birthday, which you can tell them about:
 
 - At 2:45pm, there is Contemplative Dance Practice at Marameo.
 - There will be a dinner around 6:30pm. Location TBA. They can join the birthday telegram group at https://t.me/+Kjf-Yc5inyI1MDRi to find out.
